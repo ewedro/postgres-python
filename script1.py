9eded8b4affdd741974b6f48dda2d8c7253fc40c
Ewelina Drożańska <dulianka@gmail.com>
2018-12-31 12:10:26 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,12 +1,22 @@
 import psycopg2
 conn = psycopg2.connect("dbname=dq user=dq")
 cur = conn.cursor()
+
+query = """
+CREATE TABLE notes (
+    id INTEGER PRIMARY KEY,
+    body TEXT,
+    title TEXT ); """
+cur.execute(query)
+conn.commit()
+
 query = """
 INSERT INTO notes 
 VALUES (1, 'Do more missions on Dataquest.', 'Dataquest reminder');
 """
 cur.execute(query)
 conn.commit()
+
 cur.execute("SELECT * from notes;")
 rows = cur.fetchall()
 print(rows)
