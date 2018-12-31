1c91e85d225ea4168009cebc0f5bd8b4cbae2896
Ewelina Drożańska <dulianka@gmail.com>
2018-12-31 12:16:56 +0100

    Update and rename db_create.py to db_create_drop.py

--- db_create_drop.py
+++ db_create_drop.py
@@ -3,4 +3,5 @@ conn = psycopg2.connect("dbname=dq user=dq")
 conn.autocommit = True
 cur = conn.cursor()
 cur.execute("CREATE DATABASE income OWNER dq;")
+cur.execute("DROP DATABASE income;")
 conn.close()
